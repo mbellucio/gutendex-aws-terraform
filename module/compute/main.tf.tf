54e2a540597f29239a39d3caf37ce006f741a46f
mbellucio <matheusbellucio@gmail.com>
2024-10-29 13:06:08 -0300

    setup lambda to save parquet on the s3 bucket

--- module/compute/main.tf.tf
+++ module/compute/main.tf.tf
@@ -4,9 +4,11 @@ variable "lambda_layer" {}
 resource "aws_lambda_function" "gutendex-lambda" {
   filename      = "lambda_function.zip"
   function_name = "gutendex-lambda"
-  handler       = "lambda_function.handler"
+  handler       = "lambda_function.lambda_handler"
   runtime       = "python3.11"
   role          = var.lambda_role
+
+  timeout = 900
 
   layers = [var.lambda_layer]
 
