b095aca68c0a88ab6a2e9e6c5ccac8f1bb9cfbfd
mbellucio <matheusbellucio@gmail.com>
2024-10-30 10:16:11 -0300

    Create crawler

--- main.tf
+++ main.tf
@@ -12,11 +12,15 @@ module "compute" {
 
   lambda_role = var.lambda_role
   lambda_layer = var.lambda_layer
+  bucket_name = var.bucket_name
+  database_name = var.database_name
+  crawler_name = var.crawler_name
+  crawler_role = var.crawler_role
 }
 
 module "storage" {
   source = "./module/storage"
-  bucket_name = "gutendex-bucket"
-  database_name = "glue-gutendex-db"
+  bucket_name = var.bucket_name
+  database_name = var.database_name
 }
 
--- module/compute/main.tf
+++ module/compute/main.tf
@@ -1,5 +1,9 @@
 variable "lambda_role" {}
 variable "lambda_layer" {}
+variable "bucket_name" {}
+variable "database_name" {}
+variable "crawler_name" {}
+variable "crawler_role" {}
 
 resource "aws_lambda_function" "gutendex-lambda" {
   filename      = "lambda_function.zip"
@@ -15,3 +19,21 @@ resource "aws_lambda_function" "gutendex-lambda" {
   source_code_hash = filebase64sha256("./lambda_function.zip")
 } 
 
+resource "aws_glue_crawler" "example_crawler" {
+  database_name = var.database_name
+  name          = var.crawler_name
+  role          = var.crawler_role
+
+  s3_target {
+    path = "s3://${var.bucket_name}/data"
+  }
+
+  configuration = jsonencode({
+    Version = 1.0
+    Grouping = {
+      TableGroupingPolicy = "CombineCompatibleSchemas"
+    }
+  })
+
+  table_prefix = "gutendex-"
+}
